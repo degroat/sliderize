47863a1b0c3b55f04ddcd14027cfd8b0a8faff0c
Chris DeGroat <chris.degroat@announcemedia.com>
2010-05-25 23:14:57 -0500

    a demo of the plugin

---
+++ demo.php
@@ -0,0 +1,94 @@
+<?
+
+
+// This is executed when the user clicks the save button...
+if(!empty($_POST['save']))
+{
+	// $filename is where you want to save the filename to
+	// If you leave the filename blank, the image will load in your 
+	// browser instead of save on the server
+	$filename = "";
+	save_resized_image($filename, $_POST);
+	exit;
+}
+
+function save_resized_image($filename, $values)
+{
+	$url = $values['url'];
+	$width = $values['width'];
+	$height = $values['height'];
+	$target_height = $values['target_height'];
+	$target_width = $values['target_width'];
+	$image_height = $values['image_height'];
+	$image_width = $values['image_width'];
+	$top = $values['top'];
+	$left = $values['left'];
+
+	 // create an image of the given filetype
+	if (strpos($url,".jpg") !== false or strpos($url,".jpeg") !== false) 
+	{
+       		$original = ImageCreateFromJpeg($url);
+       		$extension = ".jpg";
+   	} 
+	elseif (strpos($url,".png") !== false) 
+	{
+       		$original = ImageCreateFromPng($url);
+       		$extension = ".png";
+   	}
+	else
+	{
+		// don't want to support GIF's because they suck
+		return FALSE;
+	}
+
+	$resized = imagecreatetruecolor($target_width, $target_height);
+	imagecopyresampled ($resized, $original, 0, 0, $left , $top, $width , $height , $image_width , $image_height );
+
+	switch ($extension)
+   	{
+       		case ".jpg":
+       		{
+			if($filename == "") 
+			{
+				header('Content-Type: image/jpeg');
+			}
+           		return imagejpeg($resized, $filename, 100);
+       		}
+      		case ".png":
+           	{
+			if($filename == "") 
+			{
+				header('Content-Type: image/png');
+			}
+          		return imagepng($resized, $filename);
+       		}
+  	}
+
+	// If we got here, something didn't go right
+	return FALSE;
+}
+
+
+?>
+<html>
+	<head>
+		<title>Sliderize Demo</title>
+		<!-- JQUERY and JQUERY UI ARE REQUIRED FOR SLIDERIZE -->
+		<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" media="screen" rel="stylesheet" type="text/css" /> 
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script> 
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script> 
+		<!-- SLIDERIZE -->
+		<script type="text/javascript" src="jquery.sliderize.js"></script> 
+		<!-- HOW TO USE SLIDERIZE -->
+		<script>
+			$(document).ready(function() 
+			{
+				var image = 'http://farm5.static.flickr.com/4029/4534348997_c75703432a_b.jpg';
+				$("#sliderize").sliderize(image, {height:300, width:545});
+			});
+		</script>
+	</head>
+<body>
+	<div id="sliderize"></div>
+</body>
+</html>
